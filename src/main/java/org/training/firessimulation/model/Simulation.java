a9f2c1f50be4aed527d3ebc6c2b4a200977e5077
pheonix_23 <hedahdia.faycel@gmail.com>
2025-03-31 15:16:11 +0200

    Define Simulation class that represent actual state of Grid

--- src/main/java/org/training/firessimulation/model/Simulation.java
+++ src/main/java/org/training/firessimulation/model/Simulation.java
@@ -1,4 +1,12 @@
 package org.training.firessimulation.model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Simulation {
+    private Cell[][] grid;
+    private int step;
+    private boolean active;
 }
