7fdec3e3cbe5e7265f3c49831ce316bb2bc4c0cd
pheonix_23 <hedahdia.faycel@gmail.com>
2025-03-31 15:14:40 +0200

    reformating Launcher class

--- src/main/java/org/training/firessimulation/FiresSimulationApplication.java
+++ src/main/java/org/training/firessimulation/FiresSimulationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FiresSimulationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FiresSimulationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FiresSimulationApplication.class, args);
+    }
 
 }
